371995aeddee26dab3e0e0a92a171e29efca77fe
Yurio Dwi LS <rea.riera@gmail.com>
2023-10-04 04:04:51 +0700

    potfolio page transition

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,14 +5,30 @@ import Image3 from "../img/portfolio/3.png";
 import Image4 from "../img/portfolio/4.png";
 //import link
 import { Link } from "react-router-dom";
+//import motion
+import { motion } from "framer-motion";
+//import transition
+import { transition1 } from "../transition";
 
 const Portfolio = () => {
   return (
-    <section className="section">
+    <motion.section
+      initial={{ opacity: 0, y: "20%" }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: "20%" }}
+      transition={transition1}
+      className="section"
+    >
       <div className="container mx-auto h-full relative">
         <div className="flex flex-col lg:flex-row h-full items-center justify-start gap-x-24 text-center lg:text-left pt-24 ">
           {/* text */}
-          <div className="flex flex-col lg:items-start">
+          <motion.div
+            initial={{ opacity: 0, y: "-60%" }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: "-60%" }}
+            transition={transition1}
+            className="flex flex-col lg:items-start"
+          >
             <h1 className="h1">Portfolio</h1>
             <p className="mb-12 max-w-sm">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
@@ -29,7 +45,7 @@ const Portfolio = () => {
             <Link to={"/contact"} className="btn mb-[30px] mx-auto lg:mx-0">
               Hire me
             </Link>
-          </div>
+          </motion.div>
           {/* image grid */}
           <div className="grid grid-cols-2 lg:gap-2">
             {/* image */}
@@ -64,7 +80,7 @@ const Portfolio = () => {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
